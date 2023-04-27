9bb3f546d4589653d7070ed57595fbbe911b1f05
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-04-27 11:12:20 +0200

    added scripts for merging shapes and uploading the ontologies and shapes to the gxfs catalog

---
+++ merge_shapes.py
@@ -0,0 +1,22 @@
+import os
+
+shape_base_folder = "/home/mbuskies/gxfs/service-characteristics/yaml2shacl/"
+shape_output_path = os.path.join(shape_base_folder, "mergedShapes.ttl")
+
+prefixes = set()
+shape_defs = []
+
+for root, dirs, files in os.walk(shape_base_folder, topdown=False):
+    for name in files:
+        with open(os.path.join(root, name), "r") as f:
+            shape_def = []
+            for l in f.readlines():
+                if l.startswith("@prefix"):
+                    prefixes.add(l)
+                else:
+                    shape_def.append(l)
+            shape_defs.append("".join(shape_def))
+
+with open(shape_output_path, "w") as f:
+    f.writelines(prefixes)
+    f.writelines(shape_defs)
---
+++ upload_schemas_to_catalog.py
@@ -0,0 +1,69 @@
+from icecream import ic
+from utils import get_access_token, checkResponse
+import os
+import requests
+import json
+
+catalog_api_base = "http://localhost:8081"
+oauth_url = "https://sso.common.merlot-education.eu/realms/gxfscatalog/protocol/openid-connect/token"
+oauth_client_secret = os.getenv('OAUTH2_CLIENT_SECRET')
+oauth_user = "gxfscatalog"
+oauth_pass = os.getenv('OAUTH2_PASS')
+
+ontology_base_folder = "/home/mbuskies/gxfs/service-characteristics/yaml2ontology/"
+merged_shapes_path = "/home/mbuskies/gxfs/service-characteristics/yaml2shacl/mergedShapes.ttl"
+
+# retrieve oauth2 access token
+ic("Retrieving access token from oauth2 server")
+response = get_access_token(oauth_url,
+                            "federated-catalogue",
+                            oauth_client_secret, oauth_user, oauth_pass)
+checkResponse(response)
+access_token = response.json()["access_token"]
+ic(access_token)
+
+# retreive stored data in the catalog and clean existing ontologies/shapes
+ic("Retrieving stored schemas in catalog")
+response = requests.get(catalog_api_base + "/schemas", headers={'Authorization': 'Bearer ' + access_token,
+                                                                     "accept": "application/json"})
+checkResponse(response)
+schemas_json = json.loads(response.text)
+
+if schemas_json["ontologies"]:
+    for ontology in schemas_json["ontologies"]:
+        print("Deleting Ontology", ontology)
+        response = requests.delete(catalog_api_base + "/schemas/" + ontology.replace(":", "%3A").replace("/", "%2F")
+                                   .replace("#", "%23")
+                                   , headers={'Authorization': 'Bearer ' + access_token,
+                                                                        "accept": "application/json"})
+        checkResponse(response)
+
+if schemas_json["shapes"]:
+    for shape in schemas_json["shapes"]:
+        print("Deleting Shape", shape)
+        response = requests.delete(catalog_api_base + "/schemas/" + shape,
+                                   headers={'Authorization': 'Bearer ' + access_token,
+                                              "accept": "application/json"})
+        checkResponse(response)
+
+# upload the new available ontologies and shapes
+for root, dirs, files in os.walk(ontology_base_folder, topdown=False):
+   for name in files:
+       print("Uploading Ontology ", name)
+       with open(os.path.join(root, name), "rb") as f:
+           response = requests.post(catalog_api_base + "/schemas",
+                                      headers={'Authorization': 'Bearer ' + access_token,
+                                               "accept": "application/json",
+                                               "Content-Type":"text/turtle"},
+                                    data=f.read())
+           checkResponse(response, valid_response_code=201)
+
+
+print("Uploading Shape ", merged_shapes_path)
+with open(merged_shapes_path, "rb") as f:
+   response = requests.post(catalog_api_base + "/schemas",
+                            headers={'Authorization': 'Bearer ' + access_token,
+                                     "accept": "application/json",
+                                     "Content-Type": "text/turtle"},
+                            data=f.read())
+   checkResponse(response, valid_response_code=201)
