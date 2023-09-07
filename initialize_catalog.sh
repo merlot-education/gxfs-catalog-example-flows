67a09806fa11fd0638d25812a8a43d1a63adebc8
Marc B <125284318+M-Busk@users.noreply.github.com>
2023-09-07 14:22:58 +0200

    add flow to build signer if it does not exist yet

--- initialize_catalog.sh
+++ initialize_catalog.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+if ! [ -f "./gxfsTest-0.1.0-jar-with-dependencies.jar" ]; then
+    git clone https://gitlab.com/gaia-x/data-infrastructure-federation-services/cat/fc-tools/signer
+    cd signer
+    git apply ../signer.patch
+    mvn clean install
+    mv target/gxfsTest-0.1.0-jar-with-dependencies.jar ../
+    cd ..
+    rm -rf signer
+fi
+
 python3 delete_orgas_from_catalog.py
 # python3 delete_offerings_from_catalog.py
 python3 upload_schemas_to_catalog.py
